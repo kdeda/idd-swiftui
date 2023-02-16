4d7af2ee6df8a6388dce7e8122c3aec4b09cb0b8
Klajd Deda <kdeda@id-design.com>
2023-02-16 02:07:20 -0500

    Added more re-usable components

---
+++ Sources/IDDSwiftUI/CapsuleButtonStyle.swift
@@ -0,0 +1,65 @@
+//
+//  CapsuleButtonStyle.swift
+//  IDDSwiftUI
+//
+//  Created by Klajd Deda on 11/19/22.
+//  Copyright (C) 1997-2023 id-design, inc. All rights reserved.
+//
+
+import SwiftUI
+
+public struct CapsuleButtonStyle: ButtonStyle {
+    public init() {
+    }
+
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(.subheadline)
+            .frame(height: 18)
+            .padding([.leading, .trailing], 8)
+            .background(Color.white)
+            .foregroundColor(Color.black)
+            .cornerRadius(9)
+            .overlay(
+                RoundedRectangle(cornerRadius: 9)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+    }
+}
+
+struct CapsuleButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Text("Bla Bla ...")
+            Spacer()
+            Button(action: {
+                // viewStore.send(.stopMeasuring(viewStore.folderURL))
+            }) {
+                Text("stop")
+            }
+            .buttonStyle(CapsuleButtonStyle())
+            .help("Will stop measuring this folder")
+        }
+        .padding(20)
+        .frame(width: 480, height: 320)
+        .background(Color(NSColor.windowBackgroundColor))
+        .environment(\.colorScheme, .light)
+
+        VStack {
+            Text("Bla Bla ...")
+            Spacer()
+            Button(action: {
+                // viewStore.send(.stopMeasuring(viewStore.folderURL))
+            }) {
+                Text("stop")
+            }
+            .buttonStyle(CapsuleButtonStyle())
+            .help("Will stop measuring this folder")
+        }
+        .padding(20)
+        .frame(width: 480, height: 320)
+        .background(Color(NSColor.windowBackgroundColor))
+        .environment(\.colorScheme, .dark)
+    }
+}
