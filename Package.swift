97cb913940bacc8a93c3a387812a6299e26521fb
Klajd Deda <kdeda@id-design.com>
2023-03-09 07:28:11 -0500

    Moving from SwiftCommons

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.2.3"),
-        .package(url: "https://github.com/kdeda/idd-swift.git", from: "1.0.4")
+        .package(url: "https://github.com/kdeda/idd-swift.git", from: "1.0.6")
     ],
     targets: [
         .target(
---
+++ Sources/IDDSwiftUI/NSColor.swift
@@ -0,0 +1,33 @@
+//
+//  NSColor.swift
+//  IDDSwiftUI
+//
+//  Created by Klajd Deda on 6/3/19.
+//  Copyright (C) 1997-2023 id-design, inc. All rights reserved.
+//
+
+#if os(macOS)
+
+import Cocoa
+
+public extension NSColor {
+    /**
+     Poor attempt at coloring ...
+     */
+    static var windowBackground: NSColor {
+        var rv = NSColor.init(red: 236.0/255.0, green: 236.0/255.0, blue: 236.0/255.0, alpha: 1.0)
+        
+        if NSApplication.shared.isDarkMode {
+            rv = NSColor.init(red: 54.0/255.0, green: 54.0/255.0, blue: 54.0/255.0, alpha: 1.0)
+        }
+        return rv
+    }
+
+    static var magicBlue: NSColor {
+        // magic blue for now
+        //
+        return NSColor.init(red: 0.0/255.0, green: 154.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    }
+}
+
+#endif
